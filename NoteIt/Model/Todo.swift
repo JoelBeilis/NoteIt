73ff7b2861fa8aba63f80ccc7b4a62e82185c88e
Joel Beilis <joel.beilis@gmail.com>
2024-11-28 21:19:01 -0500

    think its time to commit
    i have issues

--- NoteIt/Model/Todo.swift
+++ NoteIt/Model/Todo.swift
@@ -13,4 +13,32 @@ class Todo {
     private(set) var taskID: String = UUID().uuidString
     var task: String
     var isCompleted: Bool = false
+    var priority: Priority = Priority.normal
+    var lastUpdated: Date = Date.now
+    
+    init(taskID: String, task: String, isCompleted: Bool, priority: Priority) {
+        self.taskID = taskID
+        self.task = task
+        self.isCompleted = isCompleted
+        self.priority = priority
+    }
 }
+
+enum Priority: String, Codable, CaseIterable {
+    case normal = "Normal"
+    case medium = "Medium"
+    case high = "High"
+    
+    var color: Color {
+        switch self {
+        case .normal:
+            return .green
+        case .medium:
+            return .yellow
+        case .high:
+            return .red
+        }
+    }
+    
+}
+
---
+++ NoteIt/View/Home.swift
@@ -0,0 +1,19 @@
+//
+//  Home.swift
+//  NoteIt
+//
+//  Created by Joel Beilis on 2024-11-28.
+//
+
+import SwiftUI
+import SwiftData
+
+struct Home: View {
+    var body: some View {
+        Text("Sup Bozo")
+    }
+}
+
+#Preview {
+    ContentView()
+}
